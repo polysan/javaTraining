3959cb1bfb723b2ef5867c1c98e981e3444a2664
okamotokeita <okamoto.keita1937@gmail.com>
2023-06-20 23:57:25 +0900

    Runnableインターフェース

--- src/chapter3/Main.java
+++ src/chapter3/Main.java
@@ -4,10 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		Thread t = new SampleThread();
-		//		startメソッドで新しいスレッドが実行される
+		// startメソッドで新しいスレッドが実行される
 		t.start();
 
 		System.out.println("main");
+
+		// RunnableのインスタンスをThreadのコンストラクタに渡すことで新しいスレッドを作る
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("sub");
+			}
+		});
+        // Runnableは関数型インターフェースなのでラムダ式で書ける
+		Thread t3 = new Thread(() -> {
+			System.out.println("sub");
+		});
+
 	}
 
 }
