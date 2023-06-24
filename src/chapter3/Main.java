293dd612cca7e8e7eb9a94844cbe90f0d519e90b
okamotokeita <okamoto.keita1937@gmail.com>
2023-06-24 23:18:01 +0900

    スレッドプールを使ったマルチスレッド処理

--- src/chapter3/Main.java
+++ src/chapter3/Main.java
@@ -1,5 +1,8 @@
 package chapter3;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -16,10 +19,46 @@ public class Main {
 				System.out.println("sub");
 			}
 		});
-        // Runnableは関数型インターフェースなのでラムダ式で書ける
+		// Runnableは関数型インターフェースなのでラムダ式で書ける
 		Thread t3 = new Thread(() -> {
 			System.out.println("sub");
 		});
+
+		NewCashedThreadPool();
+	}
+
+	private static void NewCashedThreadPool() {
+		// スレッドプールを利用したマルチスレッド処理
+		ExecutorService exec = Executors.newCachedThreadPool();
+		Runnable test = () -> {
+			System.out.println(Thread.currentThread().getId());
+		};
+
+		try {
+			for (int i = 0; i < 5; i++) {
+				exec.submit(test);
+			}
+
+			Thread.sleep(1 * 10000);
+
+			System.out.println("10秒後");
+
+			for (int i = 0; i < 5; i++) {
+				exec.submit(test);
+			}
+
+			Thread.sleep(7 * 10000);
+
+			System.out.println("70秒後");
+
+			for (int i = 0; i < 5; i++) {
+				exec.submit(test);
+			}
+
+		} catch (InterruptedException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 
 	}
 
