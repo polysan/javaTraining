51927a7109e60b6484ffb5790fda13025c7f9b6f
okamotokeita <okamoto.keita1937@gmail.com>
2023-07-31 00:29:08 +0900

    ジェネリクスの非変性

--- src/chapter7/Main.java
+++ src/chapter7/Main.java
@@ -6,5 +6,8 @@ public class Main {
 		// ジェネリクス：<T>に自分で型を指定できる
 		Currency<Double> currency = new Currency<Double>(7.23);
 		System.out.println(currency.getAmount());
+		
+		// 非変性なのでコンパイルエラーとなる
+//		List<Object> a = new ArrayList<String>();
 	}
 }
