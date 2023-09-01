d33a5b3fddf85d7ce176615061cb2c55ed55086c
okamotokeita <okamoto.keita1937@gmail.com>
2023-09-01 23:43:03 +0900

    メモ更新

---
+++ src/chapter9/Main.java
@@ -0,0 +1,13 @@
+package chapter9;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try (sc;) {
+			
+		}
+	}
+}
