baa49c730f2d79c8155051edb55448c6a496b9ad
okamotokeita <okamoto.keita1937@gmail.com>
2023-09-02 00:25:49 +0900

    セキュリティポリシーの設定

---
+++ src/chapter12/Main.java
@@ -0,0 +1,28 @@
+package chapter12;
+
+import java.io.FilePermission;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.Permissions;
+import java.security.Policy;
+
+public class Main {
+
+	@SuppressWarnings({ "removal", "deprecation" })
+	public static void main(String[] args) throws IOException {
+
+		Permissions permissions = new Permissions();
+		permissions.add(new FilePermission("/", "read"));
+		Policy.setPolicy(new SamplePolisy(permissions));
+
+		// セキュリティポリシーに沿ったセキュリティ機構が有効になる
+		System.setSecurityManager(new SecurityManager());
+
+		// ルートディレクトリのファイルとディレクトリを表示
+		Files.list(Paths.get("/")).forEach((p) -> {
+			System.out.println(p.getFileName());
+		});
+	}
+
+}
---
+++ src/chapter12/SamplePolisy.java
@@ -0,0 +1,21 @@
+package chapter12;
+
+import java.security.Permission;
+import java.security.PermissionCollection;
+import java.security.Policy;
+import java.security.ProtectionDomain;
+
+@SuppressWarnings("removal")
+public class SamplePolisy extends Policy {
+
+	private final PermissionCollection permissions;
+
+	public SamplePolisy(PermissionCollection permissions) {
+		this.permissions = permissions;
+	}
+
+	@Override
+	public boolean implies(ProtectionDomain domaijn, Permission permission) {
+		return permissions.implies(permission);
+	}
+}
